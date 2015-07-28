c8a449b4ae9e832e252f78ad873328f78a69c44f
Mikel Blake <mikel.blake@gmail.com>
2015-07-28 16:39:09 -0600

    working on addPost function

--- public/app/addContent/addContentCtrl.js
+++ public/app/addContent/addContentCtrl.js
@@ -1,9 +1,13 @@
 angular.module('groupProject')
 
 //////////// Show modal on button click ////////////
-.controller('addContentCtrl', ['$scope', function($scope) {
+.controller('addContentCtrl', ['$scope', 'addContentService', function($scope, addContentService) {
   $scope.modalShown = false;
   $scope.toggleModal = function() {
     $scope.modalShown = !$scope.modalShown;
   };
+
+  $scope.addPost = function(newPost){
+  	addContentService.addPost(newPost);
+	};
 }]);
