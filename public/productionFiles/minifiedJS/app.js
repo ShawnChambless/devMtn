5e05d2b11e9d4e7ba8dd5deee3ae8ea91716ef33
Derek Allen <derekallen2020@gmail.com>
2015-07-28 09:46:00 -0600

    Login page done with the exception of a link to the modal and logining in to further pages

--- public/productionFiles/minifiedJS/app.js
+++ public/productionFiles/minifiedJS/app.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-angular.module("groupProject",["ngRoute"]).config(["$routeProvider",function(o){o.when("/Login",{templateUrl:"productionFiles/html/Login/LoginTmpl.html",controller:"LoginCtrl"}).otherwise("/Login")}]);
-=======
-angular.module("groupProject",["ngRoute"]).config(["$routeProvider",function(n){n.when("/content",{templateUrl:"productionFiles/html/contentLanding/contentLandingTmpl.html",controller:"contentLandingCtrl"}).otherwise("/content")}]);
->>>>>>> 3fe9378d4decd719956b682fc7b8ed4a2688cb31
+angular.module("groupProject",["ngRoute"]).config(["$routeProvider",function(n){n.when("/Login",{templateUrl:"productionFiles/html/Login/LoginTmpl.html",controller:"LoginCtrl"}).otherwise("/Login").when("/content",{templateUrl:"productionFiles/html/contentLanding/contentLandingTmpl.html",controller:"contentLandingCtrl"})}]);
