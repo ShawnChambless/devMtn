9d3647d235685c1ef3a766e54e026d268b811017
Mikel Blake <mikel.blake@gmail.com>
2015-07-28 16:06:15 -0600

    ng-models on modal

--- api/models/postSchema.js
+++ api/models/postSchema.js
@@ -2,11 +2,11 @@ var mongoose    = require( 'mongoose' ) ;
     // userSchema  = require( './userSchema.js' ) ;
 
 var userSchema = {
-  type:         { type: String, required: true, enum: ['video', 'article', 'code'] } ,
+  type:         { type: String, required: true, enum: ['video', 'article', 'image'] } ,
   url:          { type: String, required: true } ,
   title:        { type: String, required: true } ,
   desc:         { type: String, required: true } ,
-  cat:          { type: String, required: true, enum: ['html', 'css', 'javascript', 'angular'] } ,
+  cat:          { type: String, required: true, enum: ['html', 'css', 'javascript', 'angular', 'mongo', 'mongoose', 'node', 'express', 'react'] } ,
   tags:         [ { type: String } ] ,
   user:         { type: mongoose.Schema.Types.ObjectId, ref: 'User' } ,
   isApproved:   { type: Boolean, required: true, default: false } ,
