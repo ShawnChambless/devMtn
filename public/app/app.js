96795b1f5aa56feaa6f106bae407290cdbeb35fa
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-28 13:03:48 -0600

    merge contentLanding with master

--- public/app/app.js
+++ public/app/app.js
@@ -1,26 +1,27 @@
 angular.module('groupProject', ['ui.router'])
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-   $urlRouterProvider.otherwise('/login');
 
-   $stateProvider
-   .state('login', {
-       url: '/login'
-   })
-   .state('home', {
-       url: '/home',
-       templateUrl: 'app/contentLanding/contentLandingTmpl.html',
-       controller: 'contentLandingCtrl'
-   })
-   .state('profile',  {
-       url: '/profile'
-   })
-   .state('admin', {
-       url: '/admin'
-   })
-   .state('category', {
-       url: '/category'
-   })
-   .state('bounty', {
-       url: '/bounty'
-   });
+    $urlRouterProvider.otherwise('/login')
+
+    $stateProvider
+    .state('login', {
+        url: '/login'
+    })
+    .state('home', {
+        url: '/home',
+        templateUrl: 'app/contentLanding/contentLandingTmpl.html',
+        controller: 'contentLandingCtrl'
+    })
+    .state('profile',  {
+        url: '/profile'
+    })
+    .state('admin', {
+        url: '/admin'
+    })
+    .state('category', {
+        url: '/category'
+    })
+    .state('bounty', {
+        url: '/bounty'
+    });
 }]);
