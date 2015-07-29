04b350d3625c7b510ab30f30ab71ebe3c57a7fa7
Derek Allen <derekallen2020@gmail.com>
2015-07-29 15:16:16 -0600

    added content to login modal and also started on user login

--- public/app/Login/LoginCtrl.js
+++ public/app/Login/LoginCtrl.js
@@ -1,9 +1,32 @@
 angular.module('groupProject')
-.controller('LoginCtrl', ['$scope', function($scope) {
+.controller('LoginCtrl', ['$scope', function($scope, LoginService) {
 
     $scope.modalShown = false;
     $scope.toggleModal = function() {
       $scope.modalShown = !$scope.modalShown;
+    };
+
+    $scope.createUser = function(email, password) {
+      console.log('Brah, its user and email stuff', email, password);
+      LoginService.createUser(email, password).then(function(data) {
+        console.log('Duuude, you are a new user!', data);
+
+        $scope.email = "";
+        $scope.password = "";
+      });
+    };
+
+    $scope.loginUser = function(email, password) {
+      LoginService.loginUser(email, password).then(function(data) {
+        console.log('Bruh you are logged in!', data);
+
+        $scope.email = "";
+        $scope.password = "";
+      })
+      .catch(function(err){
+        console.log(err);
+        $scope.password = "";
+      });
     };
 
 
--- public/app/Login/LoginService.js
+++ public/app/Login/LoginService.js
@@ -1,4 +1,37 @@
 angular.module('groupProject')
 .service('LoginService', ['$http', function($http) {
     this.test2 = 'Test from service';
-}])
+
+    this.createUser = function(email, password) {
+      var dfd = $q.defer();
+      $http({
+        method: "POST",
+        url: '/auth/local/signup',
+        data: {
+          email: email,
+          password: password
+        }
+      }).then(function(response) {
+        console.log('new user from the service', response);
+        dfd.resolve(response.data);
+      });
+    };
+
+    this.loginUser = function(email, password) {
+      var dfd = $q.defer();
+      $http({
+        method: "POST",
+        url: "/auth/local/login",
+        data: {
+          email: email,
+          password: password
+        }
+      }).then(function(response) {
+        console.log('User logging in', response);
+        dfd.resolve(response.data);
+      }, function(err) {
+        dfd.reject(err);
+      });
+      return dfd.promise;
+    };
+}]);
