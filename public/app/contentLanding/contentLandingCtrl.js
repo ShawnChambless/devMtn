d019551475f958d4c3360968d2399d59fe306672
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-29 13:40:51 -0600

    removed unnecessary function from tmpl, cleaned up functions

--- public/app/contentLanding/contentLandingCtrl.js
+++ public/app/contentLanding/contentLandingCtrl.js
@@ -2,24 +2,18 @@ angular.module('groupProject')
 .controller('contentLandingCtrl', ['$scope', 'contentLandingService', 'getPosts', function($scope, contentLandingService, getPosts) {
 
 	$scope.modalShown = false;
-
-	$scope.toggleModal = function() {
-		$scope.modalShown = !$scope.modalShown;
-	};
-
-
-	$scope.posts = getPosts.data;
 	
+	$scope.posts = getPosts.data;
+
 	$scope.getPosts = function() {
 		contentLandingService.getPosts().then(function(resp) {
 			$scope.posts = resp.data;
 		});
-	}
-
+	};
 
 	$scope.getCategoryPosts = function(cat) {
 		contentLandingService.getCategoryPosts(cat).then(function(resp) {
-			$scope.posts = resp.data
+			$scope.posts = resp.data;
 		});
 	};
 
