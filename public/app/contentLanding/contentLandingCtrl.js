30f26126f861e06d6934d1607606b45fb852ca41
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-29 17:10:28 -0600

    add contentCategories controller for use with the content specific route.

--- public/app/contentLanding/contentLandingCtrl.js
+++ public/app/contentLanding/contentLandingCtrl.js
@@ -2,19 +2,13 @@ angular.module('groupProject')
 .controller('contentLandingCtrl', ['$scope', 'contentLandingService', 'getPosts', function($scope, contentLandingService, getPosts) {
 
 	$scope.modalShown = false;
-	
+
 	$scope.posts = getPosts.data;
 
-	$scope.getPosts = function() {
-		contentLandingService.getPosts().then(function(resp) {
-			$scope.posts = resp.data;
-		});
-	};
-
-	$scope.getCategoryPosts = function(cat) {
-		contentLandingService.getCategoryPosts(cat).then(function(resp) {
-			$scope.posts = resp.data;
-		});
-	};
+	// $scope.getPosts = function() {
+	// 	contentLandingService.getPosts().then(function(resp) {
+	// 		$scope.posts = resp.data;
+	// 	});
+	// };
 
 }]);
