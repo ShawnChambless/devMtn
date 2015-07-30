06e89e11127ac44b245093d9a0524f3db2425589
Derek Allen <derekallen2020@gmail.com>
2015-07-30 10:20:53 -0600

    updated files

--- public/app/Login/LoginCtrl.js
+++ public/app/Login/LoginCtrl.js
@@ -1,16 +1,23 @@
 angular.module('groupProject')
-.controller('LoginCtrl', ['$scope', 'LoginService', function($scope, LoginService) {
+.controller('LoginCtrl', ['$scope', 'LoginService', function($scope, LoginService, $location) {
 
     $scope.modalShown = false;
     $scope.toggleModal = function() {
       $scope.modalShown = !$scope.modalShown;
     };
 
+    // $scope.comparePasswords(password, password-confirm) {
+    //   if(password === password-confirm) {
+    //     $scope.password = password
+    //   }
+    // }
+
     $scope.createUser = function(firstName, lastName, email, password) {
       console.log('Brah, its user and email stuff', firstName, lastName, email, password);
       LoginService.createUser(firstName, lastName, email, password).then(function(data) {
         console.log('Duuude, you are a new user!', data);
 
+        // $location.path('url')
         $scope.firstName = "";
         $scope.lastName = "";
         $scope.email = "";
