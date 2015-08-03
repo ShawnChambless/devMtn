0578ca54d12e1c86a53babe88dc0861848816dc4
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-03 14:22:49 -0600

    production files

--- public/productionFiles/minifiedJS/bounty/bountyService.js
+++ public/productionFiles/minifiedJS/bounty/bountyService.js
@@ -1,1 +1,1 @@
-angular.module("groupProject").service("bountyService",["$http",function(e){}]);
+angular.module("groupProject").service("bountyService",["$http",function(t){this.getBounties=function(){return t({method:"GET",url:"http://localhost:8080/bounties"})},this.getCategoryBounties=function(){return t({method:"GET",url:"http://localhost:8080/bounties/category"+cat})}}]);
