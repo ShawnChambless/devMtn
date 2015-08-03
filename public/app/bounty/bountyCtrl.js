adbcac16db03a49998bc09419b997a7ff871e09b
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-03 14:22:41 -0600

    add bounties test array, and bounties view

--- public/app/bounty/bountyCtrl.js
+++ public/app/bounty/bountyCtrl.js
@@ -1,4 +1,26 @@
 angular.module('groupProject')
 .controller('bountyCtrl', ['$scope', 'bountyService', function($scope, bountyService) {
 
+    // $scope.bounties = bounties.data;
+
+    $scope.bounties =
+    [
+        {
+            title: 'Title',
+            cat: 'Angular',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            tags: ['directives, stuff'],
+            value: 20,
+            type: 'video'
+        },
+        {
+            title: 'Title',
+            cat: 'Angular',
+            desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            tags: ['directives, stuff'],
+            value: 20,
+            type: 'video'
+        }
+    ];
+
 }]);
--- public/app/bounty/bountyService.js
+++ public/app/bounty/bountyService.js
@@ -1,4 +1,18 @@
 angular.module('groupProject')
 .service('bountyService', ['$http', function($http) {
 
+    this.getBounties = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8080/bounties'
+        });
+    };
+
+    this.getCategoryBounties = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8080/bounties/category' + cat
+        });
+    };
+
 }]);
