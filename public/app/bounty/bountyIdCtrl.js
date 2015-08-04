8b1a00e54b8b7a859e21dc3973a191f737686d71
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-04 10:40:44 -0600

    removed the ng-repeat so that Mikel can add another modal

--- public/app/bounty/bountyIdCtrl.js
+++ public/app/bounty/bountyIdCtrl.js
@@ -1,6 +1,6 @@
 angular.module('groupProject')
 .controller('bountyIdCtrl', ['$scope', 'bountyService', 'getBountyId', function($scope, bountyService, getBountyId) {
 
-    $scope.bounties = getBountyId.data;
+    $scope.bounty = getBountyId.data[0];
 
 }]);
