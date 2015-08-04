fd34b455c1a735f56d21fe578c9e1145a2487a85
Mikel Blake <mikel.blake@gmail.com>
2015-08-04 14:51:40 -0600

    added close bounty button-needs to delete

--- public/app/bounty/bountyCtrl.js
+++ public/app/bounty/bountyCtrl.js
@@ -1,6 +1,11 @@
 angular.module('groupProject')
-.controller('bountyCtrl', ['$scope', 'bountyService', 'bounties', function($scope, bountyService, bounties) {
+.controller('bountyCtrl', ['$scope', 'bountyService', 'bounties', 'LoginService', function($scope, bountyService, bounties, LoginService) {
 
     $scope.bounties = bounties.data;
+    console.log(LoginService.currentUser().isAdmin);
+    $scope.isAdmin = LoginService.currentUser().isAdmin; 
 
+    // $scope.checkAdmin = function(){
+    // 	currentUser.isAdmin = true;
+    // }
 }]);
