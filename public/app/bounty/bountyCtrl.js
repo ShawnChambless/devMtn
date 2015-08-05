22d5504c0425709519eb1f45205c1b3ba925784f
Mikel Blake <mikel.blake@gmail.com>
2015-08-05 15:53:57 -0600

    added create bounties button and functionality to the bounties page

--- public/app/bounty/bountyCtrl.js
+++ public/app/bounty/bountyCtrl.js
@@ -1,5 +1,15 @@
 angular.module('groupProject')
-.controller('bountyCtrl', ['$scope', 'bountyService', 'bounties', 'LoginService', function($scope, bountyService, bounties, LoginService) {
+.controller('bountyCtrl', ['$scope', 'bountyService', 'bounties', 'LoginService', 'adminService', function($scope, bountyService, bounties, LoginService, adminService) {
 
   $scope.bounties = bounties.data;
+
+  $scope.postBounty = function(bounty){
+		adminService.postBounty(bounty);
+			$scope.bounties.push(bounty);
+	};
+
+	$scope.modal2Shown = false;
+  $scope.toggleModal2 = function() {
+    $scope.modal2Shown = !$scope.modal2Shown;
+  };
 }]);
