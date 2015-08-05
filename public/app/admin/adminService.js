6f60e120b639a5fbf87a92a71deabd7539142c53
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-05 14:02:48 -0600

    user stuff, bounty stuff

--- public/app/admin/adminService.js
+++ public/app/admin/adminService.js
@@ -12,24 +12,29 @@ angular.module('groupProject')
   };
 
   this.approvePost = function(id){
-	  userId = user;
   	return $http({
   		method: 'PUT',
   		url: 'http://localhost:8080/api/posts/' + id,
   		data: {
 			isApproved: true,
-			user: userId._id
+			user: user._id
 		}
-  	});
+  	}).then(function(resp) {
+		return $http({
+			method: 'PUT',
+			url: 'http://localhost:8080/api/users/' + user._id + '/bounties/' + resp.data.bounty
+		});
+	});
+
   };
 
-  this.approveBountyPost = function(id) {
-	  bountyId = bounty;
-	  return $http({
-		  method: 'GET',
-		  url: 'http://localhost:8080/api/users/' + userId._id + '/bounties/' + bountyId._id
-	  });
-  };
+  // this.approveBountyPost = function(id) {
+  //  bountyId = bounty;
+  //  return $http({
+  //   method: 'GET',
+  //   url: 'http://localhost:8080/api/users/' + userId._id + '/bounties/' + bountyId._id
+  //  });
+  // };
 
   this.discardPost = function(id){
   	return $http({
