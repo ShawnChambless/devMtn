6c9608e09454e7f52788163d206c8b56273d014b
Derek Allen <derekallen2020@gmail.com>
2015-08-07 10:27:59 -0600

    styled the modals, and a few other features

--- public/productionFiles/minifiedJS/bounty/bountyCtrl.js
+++ public/productionFiles/minifiedJS/bounty/bountyCtrl.js
@@ -1,1 +1,1 @@
-angular.module("groupProject").controller("bountyCtrl",["$scope","bountyService","bounties","currentUser",function(o,n,t,u){o.bounties=t.data,o.postBounty=function(n){adminService.postBounty(n),o.bounties.push(n)},o.modal2Shown=!1,o.toggleModal2=function(){o.modal2Shown=!o.modal2Shown}}]);
+angular.module("groupProject").controller("bountyCtrl",["$scope","bountyService","bounties","adminService",function(o,n,t,u){o.bounties=t.data,o.postBounty=function(n){u.postBounty(n),o.bounties.push(n)},o.modal2Shown=!1,o.toggleModal2=function(){o.modal2Shown=!o.modal2Shown}}]);
