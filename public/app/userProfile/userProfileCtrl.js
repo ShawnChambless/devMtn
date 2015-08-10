6b4d94e1ca9aa24c8c6fb8a7801e15e6abc3193a
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-10 15:48:55 -0600

    Posts disappear when you remove them

--- public/app/userProfile/userProfileCtrl.js
+++ public/app/userProfile/userProfileCtrl.js
@@ -1,10 +1,7 @@
 angular.module('groupProject')
 .controller('userProfileCtrl', ['$scope', 'userProfileService', 'getUser', 'LoginService', 'homeService', function($scope, userProfileService, getUser, LoginService, homeService) {
-
+    // console.log(getUser)
     $scope.user = getUser;
-    // $scope.favorites = getUser.favorites;
-    // $scope.viewLater = getUser.viewLater;
-
     $scope.updateUserInfo = function(userId, newInfo) {
         userProfileService.updateUserInfo(userId, newInfo);
     };
@@ -13,32 +10,22 @@ angular.module('groupProject')
         userProfileService.deleteAccount(userId);
     };
 
-    // $scope.getFavorites = function() {
-    //     userProfileService.getFavorites().then(function(resp) {
-    //         $scope.favorites = resp.data;
-    //     });
-    // };
-    //
-    // $scope.getBounties = function(userId) {
-    //     userProfileService.getBounties(userId).then(function(resp) {
-    //         $scope.bounties = resp.data;
-    //     });
-    // };
-
-    $scope.removeFavorite = function(userId, postId) {
-        userProfileService.removeFavorite(userId, postId).then(function(resp) {
-            $scope.user.favorites.splice(resp.data._id, 1);
+    $scope.removeFavorite = function(userId, postId, index) {
+        userProfileService.removeFavorite(userId, postId).then(function() {
+            $scope.user.favorites.splice(index, 1);
         });
     };
 
-    $scope.removeviewLater = function(userId, postId) {
-        userProfileService.removeviewLater(userId, postId).then(function(resp) {
-            $scope.user.viewLater.splice(resp.data._id, 1);
+    $scope.removeviewLater = function(userId, postId, index) {
+        userProfileService.removeviewLater(userId, postId).then(function(){
+            $scope.user.viewLater.splice(index, 1);
         });
     };
 
-    $scope.deletePost = function(postId) {
-        homeService.deletePost(postId);
+    $scope.deletePost = function(postId, index) {
+        homeService.deletePost(postId).then(function(){
+            $scope.user.posts.splice(index, 1);
+        });
     };
 
 
